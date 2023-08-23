1b3ccede5d33d2c137f62c40d79ba2bdb5f23434
João Pedro Santos <121128477+oJoaoSantos@users.noreply.github.com>
2023-08-24 00:03:21 +0100

    Add files via upload

---
+++ app_send_mail/processa_envio.php
@@ -0,0 +1,125 @@
+<?php
+    
+    require "./bibliotecas/PHPMailer/Exception.php";
+    require "./bibliotecas/PHPMailer/OAuth.php";
+    require "./bibliotecas/PHPMailer/PHPMailer.php";
+    require "./bibliotecas/PHPMailer/POP3.php";
+    require "./bibliotecas/PHPMailer/SMTP.php";
+
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+
+    class Mensagem {
+        private $para = null;
+        private $assunto = null;
+        private $mensagem = null;
+        public $status = array('codigo_status' => null, 'descricao_status' => null);
+        
+        public function __set($attr, $value)
+        {
+            $this->$attr = $value;
+        }
+        public function __get($attr)
+        {
+            return $this->$attr;
+        }
+        public function mensagemValida(){
+            if (empty($this->para) || empty($this->assunto) || empty($this->mensagem)) {
+                return false;
+            } else{
+                return true;
+            }
+        }
+    }
+
+    $mensagem = new Mensagem();
+    $mensagem->__set('para', $_POST['para']);
+    $mensagem->__set('assunto', $_POST['assunto']);
+    $mensagem->__set('mensagem', $_POST['mensagem']);
+
+    // echo '<pre>';
+    // print_r($mensagem);
+    // echo '</pre>';
+    if(!$mensagem->mensagemValida()){
+        echo 'Mensagem invávida';
+        header('Location: index.php');
+    }
+
+    $mail = new PHPMailer(true);
+
+    try {
+        //Server settings
+        $mail->SMTPDebug = false;                      //Enable verbose debug output
+        $mail->isSMTP();                                            //Send using SMTP
+        $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+        $mail->Username   = 'jplstestesmail@gmail.com';                     //SMTP username
+        $mail->Password   = 'password_here';                               //SMTP password
+        $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
+        $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+        //Recipients
+        $mail->setFrom('jplstestesmail@gmail.com', 'Teste Remetente');
+        $mail->addAddress($mensagem->__get('para'));     //Add a recipient
+        // $mail->addAddress('ellen@example.com');               //Name is optional
+        // $mail->addReplyTo('info@example.com', 'Information');
+        // $mail->addCC('cc@example.com');
+        // $mail->addBCC('bcc@example.com');
+
+        //Attachments
+        // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+        // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+        //Content
+        $mail->isHTML(true);                                  //Set email format to HTML
+        $mail->Subject = $mensagem->__get('assunto');
+        $mail->Body    = $mensagem->__get('mensagem');
+        $mail->AltBody = 'É necessário utilizar um client que suporte HTMÇ para ter acesso ao conteúdo total deste mensagem';
+
+        $mail->send();
+        $mensagem->status['codigo_status'] = 1;
+        $mensagem->status['descricao_status'] = "Email enviado com sucesso!";
+    } catch (Exception $e) {
+        $mensagem->status['codigo_status'] = 2;
+        $mensagem->status['descricao_status'] = "Não foi possível enviar este email, por favor tente mais tarde. Detalhes do erro: {$mail->ErrorInfo}";
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+		<meta charset="utf-8" />
+    	<title>App Mail Send</title>
+
+    	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+	</head>
+    <body>
+        <div class="container">
+            <div class="py-3 text-center">
+				<img class="d-block mx-auto mb-2" src="logo.png" alt="" width="72" height="72">
+				<h2>Send Mail</h2>
+				<p class="lead">Seu app de envio de e-mails particular!</p>
+			</div>
+            <div class="row">
+                <div class="col-md-12">
+                    <?php
+                        if ($mensagem->status['codigo_status'] == 1) { ?>
+                            <div class="container">
+                            <h1 class="display-4 text-success">Sucesso</h1>
+                            <p><?= $mensagem->status['descricao_status'] ?></p>
+                            <a href="index.php" class="btn btn-success btn-lg mt-5 text-white">Voltar</a>
+                    <?php }
+                        if ($mensagem->status['codigo_status'] == 2) { ?>
+                            <div class="container">
+                            <h1 class="display-4 text-danger">Oops!</h1>
+                            <p><?= $mensagem->status['descricao_status'] ?></p>
+                            <a href="index.php" class="btn btn-danger btn-lg mt-5 text-white">Voltar</a>
+                    <?php } ?>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
